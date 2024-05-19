b9e56db39eb350c93c40295fbe654c7a4ab37073
Aram <aram.mart.2000@gmail.com>
2024-05-19 15:17:40 +0400

    qewe

--- app/Services/MailSender.php
+++ app/Services/MailSender.php
@@ -10,9 +10,7 @@ class MailSender
             'moderation.new_moderation',
             ['type' => $type],
             function ($message) {
-                //TODO
-//                $message->to(env('MAIL_MODERATOR'), 'ADMIN')
-                $message->to('aram.mart.2000@gmail.com', 'ADMIN')
+                $message->to(env('MAIL_MODERATOR'), 'ADMIN')
                     ->subject(env('MAIL_MODERATION_SUBJECT'));
             }
         );
